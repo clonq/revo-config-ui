89cf2639bc7c529fbca7b3b5a15bbdc4e2c9e2e3
clonq <ovi.comes@gmail.com>
2015-08-05 20:43:02 -0400

    minor template change

--- js/main.js
+++ js/main.js
@@ -2,20 +2,88 @@ clonq_revo_config_ui = {
 	init: function(config){
 		//todo: figure out the race condition and remove setTimeout
 		setTimeout(function(){
-			var template = config.template;
-			Object.keys(template).forEach(function(key){
-				var group = $('<h2/>');
-				group.html(key);
-				$('.settings').append(group);
-				var fieldList = template[key];
-				var form = generateForm(fieldList);
-				$('.settings').append(form);
-			});
+			var pageTags = generatePage(config.template);
+			$('.settings').append(pageTags);
 		}, 100);
 	}
 }
 
+function generateEntry(pair) {
+	var row = $('<div class="row"/>');
+	var leftCol = $('<div class="col-xs-3"/>');
+	var rightCol = $('<div class="col-xs-9"/>');
+	var key = Object.keys(pair)[0];
+	var value = pair[key];
+	var hasSubfields = !!value && typeof(value) === 'object';
+	if(hasSubfields) {
+		var label = $(['<label>', key, '</label>'].join(''));
+		leftCol.append(label);
+		row.append(leftCol);
+		Object.keys(value).forEach(function(key){
+			var subrow = $('<div class="row"/>');
+			var rightLeftCol = $('<div class="col-xs-3"/>');
+			var rightRightCol = $('<div class="col-xs-9"/>');
+			var label = $(['<label>', key, '</label>'].join(''));
+			var value = pair[key];
+			var isMultivalue = !!value && !hasSubfields && value.split(' ').length > 1;
+			var inputEl = $('<input/>');
+			if(isMultivalue) {
+			}
+			rightLeftCol.append(label);
+			rightRightCol.append(inputEl);
+			subrow.append(rightLeftCol);
+			subrow.append(rightRightCol);
+			rightCol.append(subrow);
+		})
+		row.append(rightCol);
+	} else {
+		var label = $(['<label>', key, '</label>'].join(''));
+		leftCol.append(label);
+		row.append(leftCol);
+		var isMultivalue = !!value && !hasSubfields && value.split(' ').length > 1;
+		var inputEl = $('<input/>');
+		if(isMultivalue) {
+			inputEl = $('<select/>');
+			value.split(' ').forEach(function(val){
+				var option = $('<option/>');
+				option.html(val);
+				inputEl.append(option);
+			});
+		}
+		rightCol.append(inputEl);
+		row.append(rightCol);
+	}
+	return row;
+}
+
+function generateSection(template, section) {
+	var sectionEl = $('<fieldset/>');
+	var sectionTitleEl = $(['<legend>',section,'</legend>'].join(''));
+	sectionEl.append(sectionTitleEl);
+	var fieldPairs = Object.keys(template[section]).map(function(key){
+		var ret = {};
+		ret[key] = template[section][key];
+		return ret;
+	});
+	fieldPairs.forEach(function(pair){
+		var entry = generateEntry(pair);
+		sectionEl.append(entry);
+	})
+	return sectionEl;
+}
+
+function generatePage(template) {
+	var ret = [];
+	var sections = Object.keys(template);
+	sections.forEach(function(section){
+		var sectionEl = generateSection(template, section);
+		ret.push(sectionEl);
+	});
+	return ret;
+}
+/*
 function generateForm(fieldList) {
+	console.log(fieldList)
 	var form = $('<form/>');
 	Object.keys(fieldList).forEach(function(fieldName){
 		var row = $('<div class="row"/>');
@@ -23,9 +91,19 @@ function generateForm(fieldList) {
 		var label = $('<label/>')
 		label.html(fieldName);
 		var field;
+		var col1 = $('<div class="col-md-3"/>');
+
 		if(fieldValue) {
+			var fieldset = $('<fieldset/>');
 			if(typeof(fieldValue) === 'object') {
-				//todo: add subfields
+				var row = $('<div class="row"/>');
+				if(!!label) {
+					var legend = $('<legend/>');
+					legend.html(label);
+					fieldset.append(legend);
+				}
+				var subform = generateForm(fieldValue)
+				fieldset.append(subform);
 			} else if(typeof(fieldValue) === 'string') {
 				if(fieldValue.split(' ').length > 1) {
 					field = $('<select/>');
@@ -36,11 +114,12 @@ function generateForm(fieldList) {
 					});
 				}
 			}
+			row.append(fieldset);
+			form.append(row);
 		} else {
 			field = $('<input/>');
+			if(!!label) col1.append(label);
 		}
-		var col1 = $('<div class="col-md-3"/>');
-		if(!!label) col1.append(label);
 		var col2 = $('<div class="col-md-9"/>');
 		if(!!field) col2.append(field);
 		row.append(col1);
@@ -49,3 +128,4 @@ function generateForm(fieldList) {
 	});
 	return form;
 }
+*/
