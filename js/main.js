67339d769ec03f22e693712c8c3a027fb1e28d0a
clonq <ovi.comes@gmail.com>
2015-08-06 22:22:16 -0400

    completed data capture for one section

--- js/main.js
+++ js/main.js
@@ -11,9 +11,18 @@ clonq_revo_config_ui = {
 				Object.keys(config.template).forEach(function(section){
 					var fields = Object.keys(config.template[section]);
 					fields.forEach(function(field){
-						var inputId = [section, '_', field].join('');
-						sectionData[section][field] = $('#'+inputId).val()
-					})
+						var hasSubfields = !!config.template[section][field] && (typeof(config.template[section][field]) === 'object');
+						if(hasSubfields) {
+							var subfields = Object.keys(config.template[section][field]);
+							subfields.forEach(function(subfield){
+								var inputId = [section, '_', field, '_', subfield].join('');
+								sectionData[section][field][subfield] = $('#'+inputId).val();
+							});
+						} else {
+							var inputId = [section, '_', field].join('');
+							sectionData[section][field] = $('#'+inputId).val()
+						}
+					});
 				});
 				alert(JSON.stringify(sectionData, null, 4));
 			})
@@ -21,14 +30,15 @@ clonq_revo_config_ui = {
 	}
 }
 
-function addPair(parent, section, key, value) {
-	var hasSubfields = !!value && typeof(value) === 'object';
-	var label = $(['<label>', key, '</label>'].join(''));
-	parent.append(label);
+function addPair(parent, section, key, label, value) {
+	var fieldId = section+'_'+key;
+	var hasSubfields = !!value && (typeof(value) === 'object');
+	var lbl = $(['<label for="'+fieldId+'">', label, '</label>'].join(''));
+	parent.append(lbl);
 	var isMultivalue = !!value && !hasSubfields && value.split(' ').length > 1;
-	var field = $('<input class="form-control" id="'+section+'_'+key+'"/>');
+	var field = $('<input class="form-control" id="'+fieldId+'"/>');
 	if(isMultivalue) {
-		field = $('<select class="form-control"/>');
+		field = $('<select class="form-control" id="'+fieldId+'"/>');
 		value.split(' ').forEach(function(val){
 			var option = $('<option/>');
 			option.html(val);
@@ -46,13 +56,13 @@ function generateEntry(section, pair) {
 		var well = $('<div class="well form-group"/>');
 		var label = $(['<label>', key, '</label><br/><br/>'].join(''));
 		well.append(label);
-		Object.keys(value).forEach(function(key){
-			var value = pair[key];
-			addPair(well, section, key, value);
+		Object.keys(value).forEach(function(subkey){
+			var value = pair[subkey];
+			addPair(well, section, key+'_'+subkey, subkey, value);
 		});
 		group.append(well);
 	} else {
-		addPair(group, section, key, value);
+		addPair(group, section, key, key, value);
 	}
 	return group;
 }
